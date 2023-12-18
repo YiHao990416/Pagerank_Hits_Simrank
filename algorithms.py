00bee5ff934d9cd2af83e45f89370f44adca5a0e
YiHao990416 <woonyihao@yahoo.com>
2023-12-18 19:20:21 +0800

    Algorithms done

--- algorithms.py
+++ algorithms.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from collections import OrderedDict
 names = ['out','in']
-graph = pd.read_csv("Project3 Dataset/graph_1.txt", header = None, names=names)
+graph = pd.read_csv("Project3 Dataset/graph_3.txt", header = None, names=names)
 
 
 
@@ -11,6 +11,7 @@ def hits_algorithms(df):
     # find the unique hubs
     unique_items = pd.unique(df[['out','in']].values.ravel('K'))
     unique_items = np.sort(unique_items)
+    n = len(unique_items)
     hubs_counts = OrderedDict()
     roots_counts = OrderedDict()
 
@@ -28,26 +29,23 @@ def hits_algorithms(df):
             roots_counts[element] = (df['in'].value_counts()[element]) 
         except:
             pass
-    
-    roots_counts = np.array(list(roots_counts.values()))
-    hubs_counts = np.array(list(hubs_counts.values()))
-    
-    print(hubs_counts)
-    print(roots_counts)
+    # print(list(roots_counts.values()))
+    hubs = np.array(list(roots_counts.values()))
+    roots = np.array(list(hubs_counts.values()))
+    for iter in range(30):
 
-    for i in range(10):
-        roots_score = (np.sum(np.square(roots_counts)))**(1/2)
-        hubs_score = (np.sum(np.square(hubs_counts)))**(1/2)
+        # new_roots = roots
+        # new_hubs = hubs
 
-        roots_counts = np.divide(roots_counts, roots_score)
-        hubs_counts = np.divide(hubs_counts, hubs_score)
+        hubs = hubs / np.sum(hubs)
+        roots = roots / np.sum(roots)        
+
     
-    print(hubs_counts)
-    print(roots_counts)
+    return hubs, roots
 
     
 
-# hits_algorithms(graph)
+hits_algorithms(graph)
 
 
 
@@ -60,65 +58,100 @@ def pagerank_algorithms(df):
     d = 0.10
     hubs_counts= OrderedDict()
     outdegree_list = []
-    pagerank = []
+    pagerank_dict ={}
+
 
     for element in unique_items:
         hubs_counts[element] = 0
         outdegree_list.append(df['out'][df['in'] == element].tolist())
-        pagerank.append(1/n)
+        pagerank_dict[element] = 1/n
         try:
             hubs_counts[element] = (df['out'].value_counts()[element])
         except:
             pass
 
-    data = {"node": unique_items,'rank': pagerank, 'outlink':list(hubs_counts.values())}
-    new_df = pd.DataFrame(data)
-
-    # def generate_pagerank(df):
-
-    #     for i,list in enumerate(outdegree_list):
-    #         score = 0
-    #         for element in list:
-    #             score += float(df['rank'][df['node'] == element].values / df['outlink'][df['node'] == element].values)
-
-    #         score = d/n + (1-d)*score
-    #         df.iloc[i,1] = score
-
-    #     return new_df    
-        
-
-        
-    def generate_pagerank(df,pagerank):
-        new_pagerank = []
-        for i in range(n):
-            df.iloc[i,1] = pagerank[i]
-        print(new_df)
-        for i,list in enumerate(outdegree_list):
-            score = 0
-            for element in list:
-                score += float((df['rank'][df['node'] == element]).values / (df['outlink'][df['node'] == element]).values)
-                
-            score = (d/n) + (1-d)*score
-            
-            new_pagerank.append(score)
-
-        return new_pagerank
-
-
+    pagerank = np.full(n, 1/n)    
     
-    pagerank1 = generate_pagerank(new_df,pagerank)
-    print(pagerank1)
+    for iter in range(30):
+        new_pagerank = np.zeros(n)
+        for i, list in enumerate(outdegree_list):
+            for j, element in enumerate(list):
+                new_pagerank[i] += pagerank_dict[element] / hubs_counts[element]
+        pagerank = (d/n) + (1-d)*new_pagerank
 
-
-
-
-
-
-
-
+        for i,element in enumerate(unique_items):
+            pagerank_dict[element] = pagerank[i]
+        
+    pagerank = pagerank/pagerank.sum()
+    
+    return pagerank
+    
 
 pagerank_algorithms(graph)
+    
 
-# def simrank_algorithms(df):
+
+def simrank_algorithms(df):
+    unique_items = pd.unique(df[['out','in']].values.ravel('K'))
+    unique_items = np.sort(unique_items)
+    n = len(unique_items)
+    c = 0.7
+    roots_counts = OrderedDict()
+    outdegree_list = []
+
+    for element in unique_items:
+        roots_counts[element] = 0
+        outdegree_list.append(df['out'][df['in'] == element].tolist())
+        try:
+            roots_counts[element] = (df['out'].value_counts()[element])
+        except:
+            pass
+
+    simrank = np.zeros((n,n))
+    for iter in range(30):
+        new_simrank = np.zeros((n,n))
+        for i in range(n):
+            for j in range(n):
+                if unique_items[i] == unique_items[j]:
+                    new_simrank[i,j] = 1.0
+                else:
+                    inlink_a = outdegree_list[i]
+                    inlink_b = outdegree_list[j]
+                    combination = []
+
+                    inlink_simrank = 0.0
+                    for a in inlink_a:
+                        for b in inlink_b:
+                            combination.append([a,b])
+                    
+                    for pair in combination:
+                        if not pair:
+                            continue
+                        else:
+                            inlink_simrank += simrank[(pair[0]-1),(pair[1]-1)]
+
+                    if roots_counts[unique_items[i]] == 0 or roots_counts[unique_items[j]] == 0 or inlink_simrank == 0:
+                        new_simrank[i,j] = 0
+                    else:
+                        new_simrank[i,j] =(c/(roots_counts[unique_items[i]]*roots_counts[unique_items[j]]))*inlink_simrank
+
+
+
+                 
+                    
+
+                    # new_simrank = (c/(roots_counts[unique_items[i]]*roots_counts[unique_items[j]]))
+        simrank = new_simrank
+    
+    return simrank
+                
+simrank_algorithms(graph)
+    
+
+
+
+
+
+
 
     
